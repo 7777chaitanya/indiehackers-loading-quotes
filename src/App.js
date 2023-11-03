a63bcc8f904f68edc18d3a4ed4663b0867303ef8
chaitanya7777 <chaitu.y25@gmail.com>
2023-11-03 20:14:03 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -35,14 +35,17 @@ function App() {
 
   return (
     <div className="App">
+      <p style={{color: 'red'}}>Note: If someone from Indiehackers want me to take this down. Please reach out to me on <a href="https://twitter.com/chaitanyaj7777" target='_blank'>Twitter</a></p>
+      <h1>All Indiehackers website loading quotes</h1>
       {quotesList.map((eachQuote, index) => {
         return (
           <div className='quoteCard'>
-            <h1>{index+1}{") "}{eachQuote.quote}</h1>
+            <h2>{index+1}{") "}{eachQuote.quote}</h2>
             <p>{eachQuote.byline}</p>
           </div>
         );
       })}
+      
     </div>
   );
 }
